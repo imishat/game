1491423d2d96cc030703cab03ee1dec4ffd6b67d
Hasan <thehasandev@gmail.com>
2023-09-05 21:45:36 +0600

    elematin

--- src/Components/Pages/Eelemination.jsx
+++ src/Components/Pages/Eelemination.jsx
@@ -4,27 +4,27 @@ import Logokisuo from "../../assets/images/images/logos.png"
 
 function Eelemination() {
   return (
-    <div className='bg-[teal] h-screen '>
-    <div className='max-w-container mx-auto'>     
-        <div className="flex items-center gap-x-7 pt-5">
-            <div className='w-[20%]  bg-white relative border  '>
+    <div className=' pb-5'>
+    <div className='w-[250px] mx-auto '>  
+        <div className="items-center flex pt-5 gap-x-3">
+            <div className='w-[50px]  bg-white relative border  '>
                 <img src={Logokisuo} alt="" />
                 <div className='w-[50px] h-[50px] -z-50 absolute bottom-[30%] -translate-y-1/2 -right-4  bg-black rotate-45'></div>
             </div>
 
-            <div className='w-[70%]'>
-                <div className='bg-[#444444] '>
-                <h1 className="pl-2 tracking-[5px] font-Oswald text-white text-[80px] font-bold">TEAM ELIMINATION</h1>
-                <h2 className="font-semibold my-3 text-3xl pl-5 text-[aqua]   font-dm text-white">DREAM TO DESTATION</h2>
+            <div>
+                <div className='bg-[#444444] w-[180px] '>
+                <h1 className="text-center tracking-[2px] font-pop text-white  text-[12px] font-bold">TEAM ELIMINATION</h1>
+                <h2 className="font-semibold my-1 w-32 mx-auto text-xl text-center  font-dm text-white">DREAM TO DESTATION</h2>
                 
-                    <div className='bg-white flex justify-center'>
-                      <h5 className="w-1/2 py-2  font-semibold text-center  text-xl font-dm  text-black">BATTLE OF THOUGHTS</h5>
-                      <h5 className="w-1/2 py-2 font-semibold text-center font-dm  text-xl  text-black  bg-[aqua]">BATTLE OF THOUGHTS</h5>
-                
+                    <div className='bg-white flex'>
+                      <h5 className="w-1/2 py-2 text-xs  font-normal text-center  font-pop  text-black">BATTLE OF THOUGHTS</h5>
+                      <h5 className="w-1/2 py-2 text-xs font-normal text-center   font-pop  text-black  bg-[aqua]">BATTLE OF THOUGHTS</h5>
                     </div> 
                 </div>
             </div>
         </div>        
+      
     </div>
     </div>
   )
